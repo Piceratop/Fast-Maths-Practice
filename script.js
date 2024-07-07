54672933ecf62e867bcabad6565c6431fda1c3f8
Piceratop Finfina <buinhatminh2k52005@gmail.com>
2024-07-07 14:09:37 +0700

    Add form

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const main = document.querySelector("main");
-let webStatus = "practice";
 
-function changePage() {
+function changePage(webStatus = "practice") {
    switch (webStatus) {
       case "practice":
          main.innerHTML = `
             <div id="practice">
                <h1>Level Selection</h1>
-               <form>Type of question: <select id="question-type">
-                  <option value="addition">Addition</option>
-                  <option value="subtraction">Subtraction</option>
-                  <option value="multiplication">Multiplication</option>
-                  <option value="division">Division</option>
-               </select></form>
+               <form>
+                  <label for="question-type">Type of question:</label>
+                  <select id="question-type">
+                     <option value="addition">Addition</option>
+                     <option value="subtraction">Subtraction</option>
+                     <option value="multiplication">Multiplication</option>
+                     <option value="division">Division</option>
+                  </select>
+                  <label for="question-length">Length of a question:</label>
+                  <input type="number" id="question-length" min="1"/>
+                  <label for="min-value">Minimum value of a number:</label>
+                  <input type="text" id="min-value"/>
+                  <label for="max-value">Maximum value of a number:</label>
+                  <input type="text" id="max-value"/>
+                  <label for="decimal-places">Maximum number of decimal places:</label>
+                  <input type="number" id="decimal-places" min="0"/>
+                  <button id="start-practice">Start</button>
+               </form>
             </div>
             `;
          break;
@@ -23,7 +34,6 @@ changePage();
 
 document.addEventListener("click", function (event) {
    if (event.target && event.target.nodeName === "LI") {
-      webStatus = event.target.innerText.toLowerCase();
-      changePage();
+      changePage(event.target.innerText.toLowerCase());
    }
 });
